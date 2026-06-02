07e9430e3c3f17e53d1a47d607028c6e7edaba79
Karlis Leon <karlisleon1@gmail.com>
2026-06-02 14:02:53 -0400

    Limpiar codigo de debug de produccion

--- api/[...path].js
+++ api/[...path].js
@@ -1,12 +1,1 @@
-let app;
-try {
-  app = require('../backend/src/app');
-} catch (err) {
-  console.error('Error al cargar el servidor:', err);
-  app = (req, res) => res.status(500).json({
-    error: 'Error de inicializacion',
-    details: err.message,
-    stack: err.stack,
-  });
-}
-module.exports = app;
+module.exports = require('../backend/src/app');
